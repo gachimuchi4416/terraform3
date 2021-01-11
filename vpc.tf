cd4327a2cd6d370af9d0e6a04a14757b5b3ff381
gachimuchi4416 <kamibayashi.dev@gmail.com>
2021-01-11 13:34:41 +0000

    プライベートサブネット用のルートテーブルを作成

--- vpc.tf
+++ vpc.tf
@@ -79,3 +79,23 @@ resource "aws_route_table_association" "public_a" {
   subnet_id      = aws_subnet.public_a.id
   route_table_id = aws_route_table.public_a.id
 }
+
+
+#プライベート用のルートテーブルを作成
+resource "aws_route_table" "private_a" {
+  vpc_id = aws_vpc.khabib.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.ngw_pub_a.id
+  }
+  tags = {
+    Name = "khabib-route-tb-priv-a"
+  }
+}
+
+#プライベートサブネットとルートテーブルの紐付け
+resource "aws_route_table_association" "private_a" {
+  subnet_id      = aws_subnet.private_a.id
+  route_table_id = aws_route_table.private_a.id
+}
