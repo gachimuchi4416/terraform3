292d80569787cfa5afaf6d52a760665acf50a783
gachimuchi4416 <kamibayashi.dev@gmail.com>
2021-01-11 12:33:25 +0000

    AZ1aに、パブリックとプライベートのサブネットを構築

--- vpc.tf
+++ vpc.tf
@@ -6,3 +6,31 @@ resource "aws_vpc" "khabib" {
     Name = "khabib-vpc"
   }
 }
+
+#WEBサーバ用Public Subnetの作成
+
+#AZ1aにてWEBサーバ用のサブネットを構築
+resource "aws_subnet" "public_1a" {
+  vpc_id                  = aws_vpc.khabib.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-northeast-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "khabib-public-1a"
+  }
+}
+
+#APサーバ用 Private Subnet
+
+#AZ1aにてAPサーバ用のサブネットを構築
+resource "aws_subnet" "private_1a" {
+  vpc_id                  = aws_vpc.khabib.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "ap-northeast-1a"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "khabib-private-1a"
+  }
+}
