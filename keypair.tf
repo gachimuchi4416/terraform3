8850ebab131973a40ed715b004a6460405adbda7
gachimuchi4416 <kamibayashi.dev@gmail.com>
2021-01-12 11:39:23 +0000

    APサーバー用の公開鍵を作成

--- keypair.tf
+++ keypair.tf
@@ -7,3 +7,13 @@ resource "aws_key_pair" "auth" {
   key_name   = "id_rsa.pub"
   public_key = data.template_file.ssh_key.rendered
 }
+
+#APサーバー用の公開鍵設定
+data "template_file" "ssh_key_priv" {
+  template = file("~/.ssh/id_rsa_priv.pub")
+}
+
+resource "aws_key_pair" "auth_priv" {
+  key_name   = "id_rsa_priv.pub"
+  public_key = data.template_file.ssh_key_priv.rendered
+}
