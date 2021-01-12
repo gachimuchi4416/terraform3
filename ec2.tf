fe837a7977fa5789e771e385c94cbe4b6326b819
gachimuchi4416 <kamibayashi.dev@gmail.com>
2021-01-12 11:51:52 +0000

    APサーバを構築

--- ec2.tf
+++ ec2.tf
@@ -10,7 +10,7 @@ data "aws_ami" "amzn2" {
   }
 }
 
-#ユーザーデータファイルを読み込む設定
+#WEBサーバへユーザーデータファイルを読み込む設定
 data "template_file" "web_shell" {
   template = file("${path.module}/web.sh.tpl")
 }
@@ -33,4 +33,30 @@ resource "aws_instance" "web" {
     Name = "khabib-web"
   }
   user_data = base64encode(data.template_file.web_shell.rendered)
+}
+
+
+#APサーバへユーザーデータファイルを読み込む設定
+data "template_file" "ap_shell" {
+  template = file("${path.module}/ap.sh.tpl")
+}
+
+#APサーバ構築
+resource "aws_instance" "ap" {
+  ami                    = data.aws_ami.amzn2.id
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.auth_priv.id
+  iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
+  subnet_id              = aws_subnet.private_a.id
+  vpc_security_group_ids = [aws_security_group.priv_a.id]
+
+  root_block_device {
+    volume_type           = "gp2"
+    volume_size           = 8
+    delete_on_termination = true
+  }
+  tags = {
+    Name = "khabib-ap"
+  }
+  user_data = base64encode(data.template_file.ap_shell.rendered)
 }
