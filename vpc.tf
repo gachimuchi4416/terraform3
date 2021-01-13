c0241ccc1c9d296273d8a257c413faa28f1a6e86
gachimuchi4416 <kamibayashi.dev@gmail.com>
2021-01-13 15:15:11 +0000

    インデント整形

--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,8 @@
 #VPCの作成
 resource "aws_vpc" "khabib" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_support = "true"
-  enable_dns_hostname = "true"
+  cidr_block          = "10.0.0.0/16"
+  enable_dns_support  = "true"
+  enable_dns_hostnames = "true"
 
   tags = {
     Name = "khabib-vpc"
