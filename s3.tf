684816c215697a31e17badd1c389cc7a59822321
gachimuchi4416 <kamibayashi.dev@gmail.com>
2021-01-14 14:47:25 +0000

    ALBログバケットを作成

--- s3.tf
+++ s3.tf
@@ -33,3 +33,57 @@ resource "aws_s3_bucket" "private_bucket" {
     }
   }
 }
+
+#ALBログ用バケット
+resource "aws_s3_bucket" "alb_access_log" {
+  bucket = "yui-yasuhiko-alb-logs-bucket"
+  acl    = "private"
+  #テスト用なので、今回は削除可能
+  force_destroy = true
+  tags = {
+    Name = "yui-yasuhiko-alb-logs-bucket"
+  }
+  lifecycle_rule {
+    enabled = true
+    id      = "alb-access-log-web"
+    expiration {
+      days = 1
+    }
+  }
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::582318560864:root"
+      },
+      "Action": "s3:PutObject",
+      "Resource": "arn:aws:s3:::yui-yasuhiko-alb-logs-bucket/*"
+    },
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "delivery.logs.amazonaws.com"
+      },
+      "Action": "s3:PutObject",
+      "Resource": "arn:aws:s3:::yui-yasuhiko-alb-logs-bucket/*",
+      "Condition": {
+        "StringEquals": {
+          "s3:x-amz-acl": "bucket-owner-full-control"
+        }
+      }
+    },
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "delivery.logs.amazonaws.com"
+      },
+      "Action": "s3:GetBucketAcl",
+      "Resource": "arn:aws:s3:::yui-yasuhiko-alb-logs-bucket"
+    }
+  ]
+}
+POLICY
+}
