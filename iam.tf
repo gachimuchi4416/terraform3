88fcafbeaa2cfb31014150da892900c4bf12f571
gachimuchi4416 <kamibayashi.dev@gmail.com>
2021-01-14 13:50:00 +0000

    EC2へ付与するIAMロールにS3フルアクセス権限を付与

--- iam.tf
+++ iam.tf
@@ -26,3 +26,29 @@ resource "aws_iam_instance_profile" "ec2_profile" {
 
   role = aws_iam_role.ec2_role.name
 }
+
+
+resource "aws_iam_policy" "policy" {
+  name        = "test-policy"
+  description = "A test policy"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "ec2:Describe*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_s3_fullaccess" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
