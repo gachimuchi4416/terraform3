a18ebd92f8677ef35d2e077b84ecf3544f489522
gachimuchi4416 <kamibayashi.dev@gmail.com>
2021-01-14 13:19:11 +0000

    AP用のとRDS用のインターナルホストゾーンとレコードの作成

--- route53.tf
+++ route53.tf
@@ -1,13 +1,53 @@
 #ホストゾーンの作成
-resource "aws_route53_zone" "yui-yasuhiko.work" {
+resource "aws_route53_zone" "khabib" {
   name = "yui-yasuhiko.work"
 }
 
 #Webサーバー用レコード設定
 resource "aws_route53_record" "web" {
-  zone_id = aws_route53_zone.yui-yasuhiko.work.zone_id
-  name    = aws_route53_zone.yui-yasuhiko.work.name
+  zone_id = aws_route53_zone.khabib.zone_id
+  name    = aws_route53_zone.khabib.name
   type    = "A"
   ttl     = "300"
   records = [aws_instance.web.public_ip]
+}
+
+#Internal ホストゾーンの作成
+resource "aws_route53_zone" "in" {
+  name = "internal"
+
+  vpc {
+    vpc_id = aws_vpc.khabib.id
+  }
+
+  tags = {
+    Name = "Internal DNS Zone"
+  }
+}
+
+#内部APサーバー用のレコード構築
+resource "aws_route53_record" "ap_in" {
+  zone_id = aws_route53_zone.in.zone_id
+  name    = "ap"
+  type    = "A"
+  ttl     = "300"
+  records = [aws_instance.ap.private_ip]
+}
+
+#内部RDSの書き込み用レコード構築
+resource "aws_route53_record" "aurora_clstr_in" {
+  zone_id = aws_route53_zone.in.zone_id
+  name    = "rds"
+  type    = "CNAME"
+  ttl     = "300"
+  records = [aws_rds_cluster.aurora_clstr.endpoint]
+}
+
+#内部RDSの読み込み用レコード構築
+resource "aws_route53_record" "aurora_clstr_ro_in" {
+  zone_id = aws_route53_zone.in.zone_id
+  name    = "rds-ro"
+  type    = "CNAME"
+  ttl     = "300"
+  records = [aws_rds_cluster.aurora_clstr.reader_endpoint]
 }
