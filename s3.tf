9ea2d80fade16c8f1afd60c9e54592ffadd72891
gachimuchi4416 <kamibayashi.dev@gmail.com>
2021-01-14 13:41:02 +0000

    テスト用のプライベートバケットを作成

--- s3.tf
+++ s3.tf
@@ -12,3 +12,24 @@ resource "aws_s3_bucket" "public_bucket" {
     max_age_seconds = 3000
   }
 }
+
+#プライベートS3バケット
+resource "aws_s3_bucket" "private_bucket" {
+  bucket = "yui-yasuhiko-private-bucket"
+  acl    = "private"
+  #テスト用なので、今回は削除可能
+  force_destroy = true
+  tags = {
+    Name = "yui-yasuhiko-private-bucket"
+  }
+  versioning {
+    enabled = true
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
