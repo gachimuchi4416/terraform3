3000f0fc299d52424b49ec0104df78f79c0eccf1
gachimuchi4416 <kamibayashi.dev@gmail.com>
2021-01-17 12:43:07 +0000

    webサーバへ付与するセキュリティグループの修正

--- ec2.tf
+++ ec2.tf
@@ -22,7 +22,7 @@ resource "aws_instance" "web" {
   key_name               = aws_key_pair.auth.id
   iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
   subnet_id              = aws_subnet.public_a.id
-  vpc_security_group_ids = [aws_security_group.pub_a.id,aws_security_group.share.id]
+  vpc_security_group_ids = [aws_security_group.pub_a.id, aws_security_group.share.id]
 
   root_block_device {
     volume_type           = "gp2"
