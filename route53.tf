97922c219141cfe1d59043bbc4e30406ddb2b4d5
gachimuchi4416 <kamibayashi.dev@gmail.com>
2021-01-17 12:44:59 +0000

    ACM検証用レコード作成

--- route53.tf
+++ route53.tf
@@ -8,8 +8,13 @@ resource "aws_route53_record" "web" {
   zone_id = aws_route53_zone.khabib.zone_id
   name    = aws_route53_zone.khabib.name
   type    = "A"
-  ttl     = "300"
-  records = [aws_instance.web.public_ip]
+  # ttl     = "300"
+  # records = [aws_instance.web.public_ip]
+  alias {
+    name                   = aws_lb.web.dns_name
+    zone_id                = aws_lb.web.zone_id
+    evaluate_target_health = true
+  }
 }
 
 #Internal ホストゾーンの作成
@@ -50,4 +55,30 @@ resource "aws_route53_record" "aurora_clstr_ro_in" {
   type    = "CNAME"
   ttl     = "300"
   records = [aws_rds_cluster.aurora_clstr.reader_endpoint]
+}
+
+#DNS検証用レコード
+# resource "aws_route53_record" "cert_validation" {
+#   zone_id = aws_route53_zone.khabib.zone_id
+#   name    = aws_acm_certificate.cert.domain_validation_options.0.resource_record_name
+#   type    = aws_acm_certificate.cert.domain_validation_options.0.resource_record_type
+#   records = [aws_acm_certificate.cert.domain_validation_options.0.resource_record_value]
+#   ttl     = 60
+# }
+
+resource "aws_route53_record" "cert_validation" {
+
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = aws_route53_zone.khabib.zone_id
 }
